3c331beefb228d98b3a83475cd094ecae5d23d8a
JoshuaTylerPearson <JoshuaTylerPearson@gmail.com>
2015-04-16 14:54:04 -0600

    adding stuff
    man

--- rom_ai_ia.py
+++ rom_ai_ia.py
@@ -1,33 +1,33 @@
-graph = {'A': set(['S', 'Z', 'T']),
-         'Z': set(['A', 'O']),
-         'O': set(['Z', 'S']),
-         'T': set(['A', 'L']),
-         'L': set(['T', 'M']),
-         'M': set(['L', 'D']),
-         'D': set(['M', 'C']),
-         'S': set(['F', 'R', 'A']),
-         'R': set(['S', 'P', 'C']),
-         'C': set(['R', 'P', 'D']),
-         'P': set(['R', 'B']),
-         'F': set(['S', 'B']),
-         'G': set(['B']),
-         'B': set(['F', 'P', 'G', 'U']),
-         'U': set(['B', 'H', 'V']),
-         'H': set(['U', 'E']),
-         'E': set(['H']),
-         'V': set(['U', 'I']),
-         'I': set(['V', 'N']),
-         'N': set(['I'])}
+graph = {'A': set(['S140', 'Z75', 'T118']),
+         'Z': set(['A75', 'O71']),
+         'O': set(['Z71', 'S151']),
+         'T': set(['A118', 'L111']),
+         'L': set(['T111', 'M70']),
+         'M': set(['L70', 'D75']),
+         'D': set(['M75', 'C120']),
+         'S': set(['F99', 'R80', 'A140']),
+         'R': set(['S80', 'P97', 'C146']),
+         'C': set(['R146', 'P138', 'D120']),
+         'P': set(['R97', 'C138', 'B101']),
+         'F': set(['S99', 'B211']),
+         'G': set(['B90']),
+         'B': set(['F211', 'P101', 'G90', 'U85']),
+         'U': set(['B85', 'H98', 'V142']),
+         'H': set(['U98', 'E86']),
+         'E': set(['H86']),
+         'V': set(['U142', 'I92']),
+         'I': set(['V92', 'N87']),
+         'N': set(['I87'])}
 
 def bfs_paths(graph, start, goal):
     queue = [(start, [start])]
     while queue:
         (vertex, path) = queue.pop(0)
         for next in graph[vertex] - set(path):
-            if next == goal:
-                yield path + [next]
+            if next[:1] == goal:
+                yield path + [next[:1]]
             else:
-                queue.append((next, path + [next]))
+                queue.append((next[:1], path + [next[:1]]))
 
 def shortest_path(graph, start, goal):
     try:
@@ -35,6 +35,35 @@ def shortest_path(graph, start, goal):
     except StopIteration:
         return None
 
+#def a_star(graph, start, goal):
+    
+
 srt = input('Starting city: ')
+
+srt = srt.upper()[:1]
+if not srt:
+    while not srt:
+        srt = input('Starting city: ')
+        srt = srt.upper()[:1]
+
 end = input('Ending city: ')
-print("shortest path: " + str(shortest_path(graph, srt.upper()[:1], end.upper()[:1])))
+
+end = end.upper()[:1]
+if not end:
+    while not end:
+        end = input('Ending city: ')
+        end = end.upper()[:1]
+
+slct = input('0: breadth first\n1: uniform cost\n')
+if not slct or not slct.isdigit():
+    while not slct or not slct.isdigit():
+        slct = input('0: breadth first\n1: uniform cost\n')
+
+slct = int(slct)
+
+if slct == 0:
+	print("shortest path: " + str(shortest_path(graph, srt, end)))
+elif slct == 1:
+	print("not currently supported")
+else:
+	print("not currently supported")
