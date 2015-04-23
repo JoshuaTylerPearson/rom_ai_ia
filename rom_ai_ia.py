8cac5d30dc7ad514ca004882f33d1086763004e7
JoshuaTylerPearson <JoshuaTylerPearson@gmail.com>
2015-04-23 13:24:58 -0600

    added comments

--- rom_ai_ia.py
+++ rom_ai_ia.py
@@ -2,6 +2,7 @@
 #	by Joshua Pearson
 #	last change 2015-04-25
 
+# graph of nodes with {'City name': set(['distance to child node+name of child node']),
 graph = {'A': set(['140S', '75Z', '118T']),
          'Z': set(['75A', '71O']),
          'O': set(['71Z', '151S']),
@@ -22,32 +23,33 @@ graph = {'A': set(['140S', '75Z', '118T']),
          'V': set(['142U', '92I']),
          'I': set(['92V', '87N']),
          'N': set(['87I'])}
+		 
 
-def bfs_path(graph, start, goal):
-    queue = [(start, [start])]
-    while queue:
-        (vertex, path) = queue.pop(0)
-        for next in graph[vertex] - set(path):
-            if next[-1:] == goal:
-                yield path + [next[-1:]]
-            else:
-                queue.append((next[-1:], path + [next[-1:]]))
+def bfs_path(graph, start, goal):		#breadth first search takes graph of nodes, stating node, and ending node
+    queue = [(start, [start])]		#make queue starting with start node with a path starting at the starting node
+    while queue:		#while the queue is not empty
+        (vertex, path) = queue.pop(0)		#pop the next node and the path it is in
+        for next in graph[vertex] - set(path):		#iterate over the children of the node 
+            if next[-1:] == goal:		#if the popped path end in the goal node:
+                yield path + [next[-1:]]		#then return the current path ending in the end node
+            else:		#otherwise:
+                queue.append((next[-1:], path + [next[-1:]]))		#append the child node to the current path and add it to the queue
                 
-def shortest_path_bfs(graph, start, goal):
+def shortest_path_bfs(graph, start, goal):		#takes the graph of nodes, stating node, and ending node then runs the breadth first search once which yields the path with the fewest nodes
     try:
         return next(bfs_path(graph, start, goal))
     except StopIteration:
         return None
 
-def uniform_cost(graph, start, goal):
-    frontier = [(0, [start])]
-    while frontier:
-        cost, path = frontier.pop()
-        if path[-1] == goal:
-            return str(path) + ": " + str(cost)
-        for n_path in graph[path[-1]] - set(path):
-            frontier.append((int(n_path[:-1]) + int(cost) , path + [n_path[-1:]]))
-        frontier.sort(reverse=True)
+def uniform_cost(graph, start, goal):		#uniform cost search takes graph of nodes, stating node, and ending node
+    frontier = [(0, [start])]		#create an array of arrays in the format cost of path, path
+    while frontier:		#while the frontier is not empty
+        cost, path = frontier.pop()		#pop the frontier separate cost of path and path
+        if path[-1] == goal:		#if the popped path end in the goal node:
+            return str(path) + ": " + str(cost)		#return the current path append to the total cost
+        for n_path in graph[path[-1]] - set(path):		#for all the child nodes of the end of the current path:
+            frontier.append((int(n_path[:-1]) + int(cost) , path + [n_path[-1:]]))		#append the frontier with the current cost, and appended the child node to the current path
+        frontier.sort(reverse=True)		#sort all paths in frontier by least cost
 
 srt = input('Starting city: ')
 srt = srt.upper()[:1]
